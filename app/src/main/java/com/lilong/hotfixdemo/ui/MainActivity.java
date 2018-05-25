f4d0520eb9bcfc86a37ae73acf53a9e82405e98a
lilong <lilong@zhangyue.com>
2018-05-25 15:56:57 +0800

    实验界面

--- app/src/main/java/com/lilong/hotfixdemo/ui/MainActivity.java
+++ app/src/main/java/com/lilong/hotfixdemo/ui/MainActivity.java
@@ -1,22 +1,36 @@
 package com.lilong.hotfixdemo.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.lilong.hotfixdemo.R;
 import com.lilong.hotfixdemo.fixed.Util;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnCallMethod;
+    private Button mBtnHotFix;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnCallMethod = (Button) findViewById(R.id.btnCallMethod);
+        mBtnCallMethod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Util.showToast();
+            }
+        });
+        mBtnHotFix = (Button) findViewById(R.id.btnHotFix);
+        mBtnHotFix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Util.showToast();
-    }
 }
